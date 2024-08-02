5e4fa698da9e52bbb65030377d788fb3b4fd28d5
Hesch <heschmat.seyedi@gmail.com>
2024-08-02 03:37:32 +0200

    App 2: Django Templates

---
+++ app2_templates/movies/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+import random
+
+best_movies = [
+    'Creed I',
+    'Se7en',
+    'Destination Wedding',
+    'Nightmare Alley',
+    'Good Will Hunting',
+    'The Intouchables',
+    "We're the Millers",
+    'The Big Sick'
+]
+
+# Create your views here.
+def home(req):
+    context = {'movie': random.choice(best_movies)}
+    return render(req, 'movies/index.html', context= context)
+
+def top_movies(req):
+    context = {'movies': best_movies}
+    return render(req, 'movies/favorites.html', context= context)
