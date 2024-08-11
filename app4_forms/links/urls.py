1be66c7d69f9e0a936fffad8bbb5402fead02e62
Hesch <heschmat.seyedi@gmail.com>
2024-08-11 19:16:45 +0200

    Add form to create links; via forms.ModelForm & django-crispy-forms

--- app4_forms/links/urls.py
+++ app4_forms/links/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import home, root_link
+from .views import home, root_link, create_link, create_link_djform
 
 urlpatterns = [
     path('', home, name= 'home'),
-    path('<str:link_slug>', root_link, name= 'root-link')
+    path('<str:link_slug>', root_link, name= 'root-link'),
+    path('link/create', create_link, name= 'create-link'),
+    path('link/add', create_link_djform, name= 'add-link')
 ]
--- app4_forms/links/views.py
+++ app4_forms/links/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
 from .models import Link
+from .forms import LinkForm
 
 # Create your views here.
 def home(req):
-    links = Link.objects.all()
-    context = {'links': links}
+    # req.GET is a dictionary-like object.
+    # .get() checks if the key - `show_all` - doesn't exists, returns 'false'.
+    show_all = req.GET.get('show_all', 'false').lower() == 'true'
+    print('GET => ', req.GET)
+    links = Link.objects.order_by('-n_clicks')
+    if not show_all:
+        # Only show the 3 most clicked links in the homepage
+        links = links[:3]
+
+    context = {'links': links, 'show_all': show_all}
     return render(req, 'links/index.html', context)
 
 def root_link(req, link_slug):
@@ -17,3 +27,23 @@ def root_link(req, link_slug):
     # we pass the parameter to the view in the template like so: (root-link) is the name for `root_link()` in urls.py
     # href="{% url 'root-link' link.slug %}
     return redirect(link.url)
+
+def create_link(req):
+    print(req.POST)
+    return render(req, 'links/create.html', {})
+
+def create_link_djform(req):
+    if req.method == 'POST':
+        # form has data
+        form = LinkForm(req.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            # Save the data & return to home page:
+            form.save()
+            return redirect(reverse('home'))
+
+    else:
+        form = LinkForm()
+    
+    context = {'form': form}
+    return render(req, 'links/create.html', context= context)
