cb2851b365206d88f22b38826a71e383a7c1f885
Hesch <heschmat.seyedi@gmail.com>
2024-08-12 14:32:01 +0200

    Class-Based-Views; implement update & delete.

--- app5_classbased_views/link_tree/urls.py
+++ app5_classbased_views/link_tree/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import LinkListView, LinkCreateView
+from .views import LinkListView, LinkCreateView, LinkUpdateView, LinkDeleteView
 
 urlpatterns = [
     path('', LinkListView.as_view(), name= 'link-list'),
     path('link/create/', LinkCreateView.as_view(), name= 'link-create'),
+    path('link/<int:pk>/update/', LinkUpdateView.as_view(), name= 'link-update'),
+    path('link/<int:pk>/delete/', LinkDeleteView.as_view(), name= 'link-delete'),
 ]
--- app5_classbased_views/link_tree/views.py
+++ app5_classbased_views/link_tree/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Profile, Link
 
@@ -26,3 +26,24 @@ class LinkCreateView(CreateView):
     # Where to redirect after creating & saving the link to the db:
     success_url = reverse_lazy('link-list')
     # template: `model_form.html` (link_form.html)
+
+class LinkUpdateView(UpdateView):
+    # create a form
+    # if GET: render the form
+    # if PUT: update & save the new values in our db
+    model = Link
+    fields = ['text', 'url']
+    success_url = reverse_lazy('link-list')
+    # template (same as post/form): `model_form.html` (link_form.html)
+
+class LinkDeleteView(DeleteView):
+    model = Link
+    # take the pi/pk of the object
+    # get_object_or_404(model, FILTER)
+    # if available => delete & forward the user to some url
+    # if not exist, show appropriate message
+    model = Link
+    success_url = reverse_lazy('link-list')
+    # by default form to submit in order to delete the item
+    # expects a template: `model_confirm_delete.html`
+    
